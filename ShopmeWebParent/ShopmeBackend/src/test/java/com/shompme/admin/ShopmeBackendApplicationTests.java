a139c52a272a24867f27c918ef0de4c2c3d3d415
pankaj <apple@apples-macbook-air.local>
2024-07-15 12:46:18 +0530

    added shopme initial structure

---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shompme/admin/ShopmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shompme.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
